fb138b632a8f26631702e7178ec5286dea51e587
Ramon Cunha <ramon.network@hotmail.com>
2023-05-13 11:51:50 -0300

    Initial commit for AWS playground

---
+++ src/main/java/com/playground/sqs/SqsPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.playground.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playground/sqs/SqsPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
